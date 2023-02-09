767e36810da4e418bdf346bde4eecda56e365ec9
Imran Ismael <imranismael.work@gmail.com>
2023-02-10 06:58:00 +0800

    added testing dataset

--- google_stock_prediction/training.py
+++ google_stock_prediction/training.py
@@ -1,8 +1,24 @@
 # Libraries
 # %% Imports
 import pandas as pd
-import os
+import numpy as np
 import matplotlib.pyplot as plt
+import datetime
+import os
+
+from tensorflow import keras
+from keras import Sequential
+from keras.layers import SimpleRNN, Dropout, Dense
+from keras.utils import plot_model
+from keras.callbacks import TensorBoard, EarlyStopping
+
+from sklearn.metrics import (
+    mean_absolute_error,
+    mean_absolute_percentage_error,
+    r2_score,
+)
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
 
 #%% # Step 1 Data Loading
 CSV_PATH = os.path.join(
@@ -48,8 +64,6 @@ df["Open"] = df["Open"].interpolate(method="polynomial", order=2)  # Order 2/3
 data = df["Open"].values
 
 #%% # Step 5 Data Preprocessing
-from sklearn.preprocessing import MinMaxScaler
-import numpy as np
 
 mms = MinMaxScaler()
 # data = mms.fit_transform(np.reshape(data, (-1,-1)))
@@ -61,27 +75,22 @@ data = mms.fit_transform(np.expand_dims(data, -1))
 #%%
 X_train, y_train = [], []
 
-for i in range(60, len(data)):
-    X_train.append(data[i - 60 : i])
+win_size = 60
+
+for i in range(len(df), len(data)):
+    X_train.append(data[i - win_size : i])
     y_train.append(data[i])
 
 # Convert list into array
 X_train = np.array(X_train)
 y_train = np.array(y_train)
 
-from sklearn.model_selection import train_test_split
-
 X_train, X_test, y_train, y_test = train_test_split(
     X_train, y_train, test_size=0.3, random_state=123
 )
 
-
 #%%
 # Step 6 Model Development
-from tensorflow import keras
-from keras.layers import SimpleRNN, Dropout, Dense
-from keras import Sequential
-from keras.utils import plot_model
 
 input_shape = np.shape(X_train)[1:]
 model = Sequential()
@@ -104,9 +113,6 @@ model.summary()
 plot_model(model, show_shapes=True)
 
 #%% # * Tensorboard callbacks
-
-import datetime
-from keras.callbacks import TensorBoard, EarlyStopping
 
 parent_folder = "tensorboard_log"
 log_dir = os.path.join(
@@ -138,18 +144,51 @@ plt.legend(["training accuracy", "validation accuracy"])
 plt.show()
 
 #%%
-from sklearn.metrics import (
-    mean_absolute_error,
-    mean_absolute_percentage_error,
-    r2_score,
-)
-
 y_true = y_test
 y_pred = model.predict(X_test)
 print(mean_absolute_error(y_true, y_pred))
 print(mean_absolute_percentage_error(y_true, y_pred))
 print(r2_score(y_true, y_pred))
 
+
+#%% Model Analysis against actual data
+
+df_test = pd.read_csv(os.path.join(os.getcwd(), 'dataset', 'Google_Stock_Price_Test.csv'))
+
+# Joining both datasets (train and test)
+dataset_tot = pd.concat((df, df_test))
+dataset_tot = dataset_tot['Open']   
+
+
+# MinMaxScaler for X
+dataset_tot = mms.transform(np.expand_dims(dataset_tot, axis=-1))
+
+
+X_actual, y_actual = [], []
+
+
+for i in range(len(df), len(dataset_tot)):  
+    X_actual.append(dataset_tot[i-win_size: i ])
+    y_actual.append(dataset_tot[i])
+
+X_actual = np.array(X_actual)
+y_actual = np.array(y_actual)
+
+#%% Testing the model with actual data
+
+y_pred_actual = model.predict(X_actual)
+
+plt.figure()
+plt.plot(y_pred_actual, color='red')
+plt.plot(y_actual, color='blue')
+plt.legend(['Predicted Stock Price', 'Actual Stock Price'])
+plt.show()
+
+#%%
+y_actual = y_test
+print(mean_absolute_error(y_actual, y_pred_actual))
+print(mean_absolute_percentage_error(y_actual, y_pred_actual))
+print(r2_score(y_actual, y_pred_actual))
 
 #%% Step 8 Model Deployment
 
