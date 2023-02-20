5beb1b15a8d8ec20b788ee95c9974300128fdbf9
Imran Ismael <imranismael.work@gmail.com>
2023-02-20 13:41:25 +0800

    fixing some stuff

--- covid_cases_prediction/imran_training.py
+++ covid_cases_prediction/imran_training.py
@@ -34,17 +34,28 @@ plt.figure()
 plt.plot(df["cases_new"])
 plt.show()
 #%% Step 3) Data Cleaning
+df["date"] = pd.to_datetime(df["date"], format="%d/%m/%Y")
+# Make sure to order by ascending date
+df = df.sort_values("date")
+
+print(df.head(20))
+
+#%%
 # Convert cases_new to numeric data type
 df["cases_new"] = pd.to_numeric(df["cases_new"], errors="coerce")
 
 # Interpolation to Handle missing values
 df["cases_new"] = df["cases_new"].interpolate(method="polynomial", order=2)
 
+print(df.info())
+print(df.tail(5))
 #%%
 
-print(df.info())
 plt.figure()
 plt.plot(df["cases_new"])
+plt.xlabel("Days")
+plt.ylabel("New Cases")
+plt.title("Daily New COVID-19 Cases in Malaysia")
 plt.show()
 
 #%% Step 4) Feature Selections
@@ -81,7 +92,7 @@ print(np.isnan(y_train).any())
 
 input_shape = np.shape(X_train)[1:]
 model = Sequential()
-model.add(LSTM(64, activation="tanh", input_shape=input_shape))
+model.add(LSTM(64, activation="relu", input_shape=input_shape, return_sequences=False))
 model.add(Dropout(0.3))
 model.add(Dense(1, activation="linear"))
 model.summary()
@@ -105,7 +116,7 @@ hist = model.fit(
     X_train,
     y_train,
     batch_size=128,
-    epochs=100,
+    epochs=300,
     validation_data=(X_test, y_test),
     callbacks=[tb_callback, es_callback],
 )
@@ -118,15 +129,21 @@ plt.plot(hist.history["val_loss"])
 plt.legend(["Training Loss", "Validation Loss"])
 plt.show()
 
-
 #%% Testing with actual dataset
-column_names = df.columns
-df_test = pd.read_csv(os.path.join(os.getcwd(), "datasets", "cases_malaysia_test.csv"), names=column_names)
+df_test = pd.read_csv(os.path.join(os.getcwd(), "datasets", "cases_malaysia_test.csv"))
+print(df_test.head())
+
+df_test["date"] = pd.to_datetime(df["date"], format="%d/%m/%Y")
+df_test["date"] = df_test["date"].dt.strftime("%Y-%m-%d")
+
+#%%
+print(df_test.head())
+#%%
 df_test["cases_new"] = pd.to_numeric(df_test["cases_new"], errors="coerce")
 df_test["cases_new"] = df_test["cases_new"].interpolate(method="polynomial", order=2)
 
 dataset_tot = pd.concat((df, df_test))
-dataset_tot = dataset_tot["cases_new"]
+dataset_tot = dataset_tot["cases_new"].values
 
 #%%
 # MinMaxScaler for X
@@ -147,7 +164,11 @@ y_pred_actual = model.predict(X_actual)
 plt.figure()
 plt.plot(y_pred_actual, color="red")
 plt.plot(y_actual, color="blue")
-plt.legend(["Predicted Stock Price", "Actual Stock Price"])
+plt.legend(["Predicted Covid Cases", "Actual Covid Cases"])
 plt.show()
 
+#%%
+print(mean_absolute_error(y_actual, y_pred_actual))
+print(mean_absolute_percentage_error(y_actual, y_pred_actual))
+print(r2_score(y_actual, y_pred_actual))
 #%% Step 8) Model Deployment
